2d9e2702e3bf8f9049e3c09f17e3e6da2add259b
karunakar bhogyari <karunakar1008@gmail.com>
2023-01-23 09:53:47 +0530

    Change title

--- src/PairingTest.WebApi/Startup.cs
+++ src/PairingTest.WebApi/Startup.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using PairingTest.WebApi.Interfaces;
 
 namespace PairingTest.WebApi
@@ -30,10 +30,10 @@ namespace PairingTest.WebApi
             {
                 app.UseDeveloperExceptionPage();
             }
-
             app.UseHttpsRedirection();
             app.UseRouting();
-            app.UseEndpoints(endpoints => {
+            app.UseEndpoints(endpoints =>
+            {
                 endpoints.MapControllers();
             });
         }
